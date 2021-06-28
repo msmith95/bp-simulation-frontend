4095cc8c0ef2e4934020a4f4ce6fc0871d0aee37
Michael Smith Jr <mj3397@gmail.com>
2021-06-27 19:09:19 -0500

    Splits up SCSS

--- src/components/navigation/TopNav.vue
+++ src/components/navigation/TopNav.vue
@@ -4,12 +4,12 @@
             <div class="flex items-center justify-between h-16">
                 <div class="flex items-center">
                     <p class="text-white">Blueprint Simulation</p>
-                    <div class="ml-10 flex items-baseline space-x-4">
+                    <div class="ml-10 flex items-baseline space-x-4 text-gray-300">
                         <router-link :to="{name: 'home'}"
-                                     class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Home
+                                     class="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" active-class="hover:bg-gray-900 bg-gray-900 text-white">Home
                         </router-link>
                         <router-link :to="{name: 'take-screener', params: {id: 'abcd-123'}}"
-                                     class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                                     class="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" active-class="hover:bg-gray-900 bg-gray-900 text-white">
                             Take Screener
                         </router-link>
                     </div>
